3b665b0583982890e31a6d6a5d1bfb89122d45f1
Victor Boctor <vboctor@gmail.com>
2017-04-08 16:38:50 -0700

    Fix SampleAuth to work with latest Auth Plugins

--- SampleAuth.php
+++ SampleAuth.php
@@ -5,7 +5,7 @@
 /**
  * Sample Auth plugin
  */
-class SampleAuthPlugin extends AuthPlugin  {
+class SampleAuthPlugin extends MantisPlugin  {
 	/**
 	 * A method that populates the plugin information and minimum requirements.
 	 * @return void
@@ -31,40 +31,57 @@ class SampleAuthPlugin extends AuthPlugin  {
 	 */
 	function hooks() {
 		$t_hooks = array(
-			'EVENT_AUTH_FLAGS' => 'auth_flags',
+			'EVENT_AUTH_USER_FLAGS' => 'auth_user_flags',
 		);
 
 		return $t_hooks;
 	}
 
-	function auth_flags( $p_event_name, AuthFlags $p_flags ) {
+	function auth_user_flags( $p_event_name, $p_args ) {
 		# Don't access DB if db_is_connected() is false.
-		# See list of flags in core/authentication_api.php auth_flags() function.
+
+		$t_username = $p_args['username'];
+
+		$t_user_id = $p_args['user_id'];
+
+		# If user is unknown, don't handle authentication for it, since this plugin doesn't do
+		# auto-provisioning
+		if( !$t_user_id ) {
+			return null;
+		}
+
+		# If anonymous user, don't handle it.
+		if( user_is_anonymous( $t_user_id ) ) {
+			return null;
+		}
+
+		$t_access_level = user_get_access_level( $t_user_id, ALL_PROJECTS );
+
+		# Have administrators use default login flow
+		if( $t_access_level >= ADMINISTRATOR ) {
+			return null;
+		}
+
+		# for everybody else use the custom authentication
+		$t_flags = new AuthFlags();
 
 		# Passwords managed externally for all users
-		$p_flags->setSetPasswordThreshold( NOBODY );
-		$p_flags->setPasswordManagedExternallyMessage( 'Passwords are no more, you cannot change them!' );
+		$t_flags->setCanUseStandardLogin( false );
+		$t_flags->setPasswordManagedExternallyMessage( 'Passwords are no more, you cannot change them!' );
 
 		# No one can use standard auth mechanism
-		$p_flags->setUserStandardLoginThreshold( NOBODY );
 
 		# Override Login page and Logout Redirect
-		$p_flags->setLoginPage( plugin_page( 'login_page', /* redirect */ true ) );
-		$p_flags->setLogoutRedirectPage( plugin_page( 'logout', /* redirect */ true ) );
+		$t_flags->setCredentialsPage( helper_url_combine( plugin_page( 'login', /* redirect */ true ), 'username=' . $t_username ) );
+		$t_flags->setLogoutRedirectPage( plugin_page( 'logout', /* redirect */ true ) );
 
 		# No long term session for identity provider to be able to kick users out.
-		$p_flags->setPermSessionEnabled( false );
-
-		# Disable anonymous access
-		$p_flags->setAnonymousEnabled( false );
-
-		# Set default access level for new signups, e.g. users rather than team members.
-		$p_flags->setSignupAccessLevel( REPORTER );
+		$t_flags->setPermSessionEnabled( false );
 
 		# Enable re-authentication and use more aggressive timeout.
-		$p_flags->setReauthenticationEnabled( true );
-		$p_flags->setReauthenticationLifetime( 10 );
+		$t_flags->setReauthenticationEnabled( true );
+		$t_flags->setReauthenticationLifetime( 10 );
 
-		return $p_flags;
+		return $t_flags;
 	}
 }
